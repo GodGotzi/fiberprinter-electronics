1209245a53db6882bdaa2a80b0e34c051c3a7b39
JanHTL <trajan20@htl-kaindorf.at>
2024-08-01 08:18:57 +0200

    better working pcb to pdf, now shortening it down more

--- workflow.py
+++ workflow.py
@@ -21,6 +21,16 @@ def make_output_directory(dir, filename):
         os.remove(dir)
     return dir
 
+pcb_layers_V1 = [
+    "F.Cu", "B.Cu", "F.Adhes", "B.Adhes", "F.Paste", "B.Paste",
+    "F.Mask", "B.Mask", "Edge.Cuts", "Margin", "In1.Cu", "In2.Cu",
+    "In3.Cu", "In4.Cu", "In5.Cu", "In6.Cu", "In7.Cu", "In8.Cu",
+    "In9.Cu", "In10.Cu", "In11.Cu", "In12.Cu", "In13.Cu", "In14.Cu",
+    "In15.Cu", "In16.Cu", "Dwgs.User", "Cmts.User", "Eco1.User",
+    "Eco2.User", "B.Fab", "F.Fab", "B.SilkS", "F.SilkS", "B.CrtYd",
+    "F.CrtYd"
+]
+
 # List of layers to include in the PDF export for PCB files
 pcb_layers_V2 = [
     "F.Cu", "B.Cu", "F.Adhes", "B.Adhes", "F.Paste", "B.Paste",  
@@ -56,6 +66,12 @@ def export_sch_to_bom(dir, filename):
 def export_pcb_to_pdf(dir, filename):
     input_file = make_input_directory(dir, ".kicad_pcb")
     output_file = make_output_directory(dir, filename)
+    run_kicad_cli_command("pdf", "pcb", input_file, output_file, "--layers", ",".join(pcb_layers_V1), "--exclude-value",  "--include-border-title", )
+
+# Function to run kicad-cli command for PCB files
+def export_pcb_to_pdf_V2(dir, filename):
+    input_file = make_input_directory(dir, ".kicad_pcb")
+    output_file = make_output_directory(dir, filename)
     run_kicad_cli_command("pdf", "pcb", input_file, output_file, "--layers", ",".join(pcb_layers_V2))
 
 # Function to run kicad-cli command for detailed PCB files
@@ -80,6 +96,7 @@ for dir in dirs:
     export_sch_to_pdf(dir, "schematic.pdf")
     export_sch_to_bom(dir, "bom.csv")
     export_pcb_to_pdf(dir, "pcb.pdf")
+    export_pcb_to_pdf_V2(dir, "pcb_V2.pdf")
     export_pcb_to_pdf_detailed(dir, "pcb_detailed.pdf")
     export_pcb_to_step(dir, "pcb.step")
     export_pcb_to_dxf(dir, "pcb.dxf")
